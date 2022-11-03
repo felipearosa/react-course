79c8d73e34031338c1730e723c4a3c760483a660
Felipe <felipe.arosa@gmail.com>
2022-11-03 11:55:50 -0300

    rendered list automatically

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -15,10 +15,9 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter selectedYear={enteredYear} onYearChange={changeGraphYear}/>
-        <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}></ExpenseItem>
-        <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}></ExpenseItem>
-        <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}></ExpenseItem>
-        <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date}></ExpenseItem>
+        {props.expenses.map(expense => {
+          return <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
+        })}
       </Card>
     </div>
   )
