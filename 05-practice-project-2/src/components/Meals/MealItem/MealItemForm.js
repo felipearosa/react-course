33ec1ca0e0694a40bf9170e6c6199bacd5d3529f
Felipe <felipe.arosa@gmail.com>
2022-11-19 13:44:29 -0300

    added refs and reducer function to update cart

--- 05-practice-project-2/src/components/Meals/MealItem/MealItemForm.js
+++ 05-practice-project-2/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,30 @@
+import { useRef, useState } from 'react';
+
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = props => {
+  const [amountIsValid, setAmountIsValid] = useState(true)
+  const inputRef = useRef();
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    const amount = inputRef.current.value;
+    const numberAmount = +amount;
+
+    if(amount.trim().length === 0 || numberAmount < 1 || numberAmount > 5)  {
+      setAmountIsValid(false)
+      return;
+    }
+
+    props.onAddItem(numberAmount)
+
+  }
+
   return (
-    <form className={classes.form}>
-      <Input id="amount" label="Amount" input={{
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input ref={inputRef} id="amount" label="Amount" input={{
         id: 'amount_' + props.id,
         type: 'number',
         min: '1',
@@ -13,6 +33,7 @@ const MealItemForm = props => {
         defaultValue: '1'
       }} />
       <button>+ Add</button>
+      {!amountIsValid && <p>Enter a valid amount (1-5)</p> }
     </form>
   )
 }
