236eecc9da4321318bba0776360f23c1dee6d44b
Felipe <felipe.arosa@gmail.com>
2022-12-01 18:45:14 -0300

    form with style added

--- 06-practice-project-3/src/components/Cart/Cart.js
+++ 06-practice-project-3/src/components/Cart/Cart.js
@@ -1,11 +1,14 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 
 import classes from './Cart.module.css'
 import Modal from '../UI/Modal'
 import CartContext from '../../store/cart-context'
 import CartItem from './CartItem'
+import Checkout from './Checkout'
 
 const Cart = props => {
+  const [isCheckingOut, setCheckingOut] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
   const totalAmout = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -16,22 +19,29 @@ const Cart = props => {
   };
 
   const cartItemAddHandler = item => {
-    cartCtx.addItem({...item, amount:1})
+    cartCtx.addItem({ ...item, amount: 1 })
   };
 
-  const cardItems = cartCtx.items.map(item => <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)} />)
+  const cardItems = cartCtx.items.map(item => <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)} />);
 
-  return(
+  const orderHandler = () => {
+    setCheckingOut(true);
+  }
+
+  const btns = <div className={classes.actions}>
+  <button onClick={props.onHideCart} className={classes['button--alt']}>Close</button>
+  {hasItems && <button onClick={orderHandler} className={classes.button}>Order</button>}
+</div>
+
+  return (
     <Modal onHideCart={props.onHideCart}>
       {cardItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmout}</span>
       </div>
-      <div className={classes.actions}>
-        <button onClick={props.onHideCart} className={classes['button--alt']}>Close</button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+      {isCheckingOut && <Checkout closeModal={props.onHideCart} />}
+      {!isCheckingOut && btns}
     </Modal>
   )
 }
