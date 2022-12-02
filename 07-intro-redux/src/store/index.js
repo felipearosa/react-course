a6a06b0ee45163ae50d88ece6f2f05056234472a
Felipe <felipe.arosa@gmail.com>
2022-12-02 11:37:49 -0300

    created js file for redux

---
+++ 07-intro-redux/src/store/index.js
@@ -0,0 +1,21 @@
+const redux = require('redux');
+
+const counterRedux = (state, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1
+    };
+  }
+
+  return state;
+};
+
+const store = redux.configureStore(reduxReducer);
+
+export default store;
