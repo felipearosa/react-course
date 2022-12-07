9e4c83b51b90d8b35abe314ce0448526362f2e82
Felipe <felipe.arosa@gmail.com>
2022-12-07 15:53:36 -0300

    updated form to go to /quotes after submit

--- 10-router-practice/src/pages/NewQuote.js
+++ 10-router-practice/src/pages/NewQuote.js
@@ -1,7 +1,12 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = quoteData => {
+    history.push('/quotes')
     console.log(quoteData);
   }
 
