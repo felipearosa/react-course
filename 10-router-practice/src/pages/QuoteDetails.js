f77f122afb263e0398fab0c9d1d5e298d7a3bdb2
Felipe <felipe.arosa@gmail.com>
2022-12-07 15:43:11 -0300

    added quote details to /quotes/:id

--- 10-router-practice/src/pages/QuoteDetails.js
+++ 10-router-practice/src/pages/QuoteDetails.js
@@ -1,16 +1,27 @@
 import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 
-import Comments from './../components/comments/Comments'
+import Comments from './../components/comments/Comments';
+import HighlightedQuote from './../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'someone', text: 'hvewnj fwen wefn fwnei' },
+  { id: 'q2', author: 'someone else', text: 'vempavmwo movwempvwe vwme' }
+]
 
 const QuoteDetails = () => {
   const params = useParams();
-  const { quoteId } = params
+  const { quoteId } = params;
+
+  const quote = DUMMY_QUOTES.find(quote => quote.id === quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>
+  }
 
   return (
     <Fragment>
-      <h1>Quote Detail page</h1>
-      <p>{quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${quoteId}/comments`}>
         <Comments />
       </Route>
