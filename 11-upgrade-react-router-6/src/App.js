a1027fccf4a4409f52c94858562810cce3808af1
Felipe <felipe.arosa@gmail.com>
2022-12-11 20:23:49 -0300

    updated

--- 11-upgrade-react-router-6/src/App.js
+++ 11-upgrade-react-router-6/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -10,17 +10,12 @@ function App() {
     <div>
       <MainHeader />
       <main>
-        <Switch>
-          <Route path='/welcome'>
-            <Welcome />
-          </Route>
-          <Route path='/products' exact>
-            <Products />
-          </Route>
-          <Route path='/products/:productId'>
-            <ProductDetail />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/" exact element={<Navigate to ="/welcome" />} />
+          <Route element={<Welcome />} path='/welcome/*' />
+          <Route element={<Products />} path='/products' />
+          <Route element={<ProductDetail />} path='/products/:productId' />
+        </Routes>
       </main>
     </div>
   );
--- 11-upgrade-react-router-6/src/pages/Welcome.js
+++ 11-upgrade-react-router-6/src/pages/Welcome.js
@@ -1,5 +1,15 @@
+import { Routes, Route, Link } from "react-router-dom";
+
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>;
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      <Link to='new-user'>New User</Link>
+      <Routes>
+        <Route path='/new-user' element={<p>Welcome New User</p>} />
+      </Routes>
+    </section>
+  )
 };
 
 export default Welcome;
