1b4b51b5d60255bbf7a73216cd116e7e7d509d0b
Felipe <felipe.arosa@gmail.com>
2022-12-19 14:33:43 -0300

    added http post request

--- 12-nextjs-project/pages/api/new-meetup.js
+++ 12-nextjs-project/pages/api/new-meetup.js
@@ -2,16 +2,25 @@ const { MongoClient } = require('mongodb')
 
 const handler = async (req, res) => {
   if (req.method === 'POST') {
-    const data = req.body
 
-    const client = await MongoClient.connect(`mongodb+srv://${proces.env.MONGO_USERNAME}:${proces.env.MONGO_PS}@cluster0.9hvl57g.mongodb.net/meetups?retryWrites=true&w=majority`);
-    const db = client.db();
+    try {
+      const data = req.body
 
-    const meetupsCollection = db.collection('meetups');
+      const client = await MongoClient.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PS}@cluster0.9hvl57g.mongodb.net/meetups?retryWrites=true&w=majority`);
+      const db = client.db();
 
-    const result = await meetupsCollection.insertOne(data)
+      const meetupsCollection = db.collection('meetups');
 
-    client.close();
+      const result = await meetupsCollection.insertOne(data)
+
+      client.close();
+
+    } catch (error) {
+      res.status(400).json({
+        message: `ERROR: ${error.message}`
+      })
+    }
+
 
     res.status(201).json({
       message: 'Data inserted'
