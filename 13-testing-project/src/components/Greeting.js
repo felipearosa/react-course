aadbfc81d8a116a2d690adb78a9b89ccd63c2171
Felipe <felipe.arosa@gmail.com>
2023-01-13 12:47:23 -0300

    tested user interaction

--- 13-testing-project/src/components/Greeting.js
+++ 13-testing-project/src/components/Greeting.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import { Fragment } from "react/cjs/react.production.min";
+
 const Greeting = () => {
-   return <div>Hello World!</div>
+  const [changedText, setChangedText] = useState(false);
+
+  const changeHandler = () => {
+    setChangedText(true)
+  }
+
+  return (
+    <Fragment>
+      <div>Hello World!</div>
+      <div>
+        {changedText && 'Changed'}
+        {!changedText && "Hasn't changed yet"}
+      </div>
+      <button onClick={changeHandler}>Change</button>
+    </Fragment>
+  )
 }
 
 export default Greeting;
